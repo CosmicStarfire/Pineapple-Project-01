83f7213b12ad8a97bf40199024f09cbd1a4b2abd
Miranda Saddleback Nanatewikamik <nanatewikamik@gmail.com>
2024-02-13 09:31:46 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.querySelector('#send').addEventListener('click', function() {
+const name = document.querySelector('#name').value;
+const phone = document.querySelector('#phone').value;
+const email = document.querySelector('#email').value;
+const message = document.querySelector('#message').value;
+
+// code to send the form data goes here.
+alert('Message sent successfully!');
+});
+
+                
